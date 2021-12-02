6dcb1f1c55d3597bd7156c274607bb798ae73fa8
arathore <arathore@splunk.com>
2021-12-02 20:14:18 +0530

    Dossier: Updating copyright comment for uniformity

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 # File: __init__.py
+#
 # Copyright (c) 2019-2021 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- dossier_connector.py
+++ dossier_connector.py
@@ -1,4 +1,5 @@
 # File: dossier_connector.py
+#
 # Copyright (c) 2019-2021 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
